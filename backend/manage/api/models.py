5202af5a9b79a574522fcf3e7503f1b116f76134
KWIZERA-CALEB <146452204+KWIZERA-CALEB@users.noreply.github.com>
2024-09-13 20:37:54 +0100

    Updated the requirements.txt file

--- backend/manage/api/models.py
+++ backend/manage/api/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 class Event(models.Model):
-    
+    title = models.CharField(max_length=255),
+    description = models.TextField(),
+    uploaded = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title,
--- backend/manage/api/views.py
+++ backend/manage/api/views.py
@@ -1,11 +1,27 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Event
+from .serializers import EventSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
         'GET /events',
+        'POST /add-event'
+        'GET /event'
     ]
 
     return Response(routes)
+
+@api_view(['POST'])
+def createNewEvent(request):
+    serializer = EventSerializer(data=request.data)
+    if request.method == 'POST':
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Event added"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response({"error": "Internal Server error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
